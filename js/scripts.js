e1e63ecb1aaffdc8ba2a26697344be95065d0155
James Lannon and Rose Sponseller <james+rose@>
2017-04-24 11:17:33 -0700

    Add initial project files, including stylesheet, Javascript file, and HTML page

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(num1, num2) {
+	return num1 + num2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+
+alert(result);
