b065599b271640a709379c1c239d31d9ecff10e6
James Lannon and Rose Sponseller <james@>
2017-04-24 14:16:55 -0700

    Add programs for simple calculator, converting celsius to fahreinheit and vice versa, prompt user for favorite items, and another program for a BMI calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,58 @@
-var add = function(num1, num2) {
-	return num1 + num2;
-};
-
-var subtract = function(num1, num2) {
-  return num1 - num2;
-};
-
-var multiply = function(num1, num2) {
-  return num1 * num2;
-};
-
-var divide = function(num1, num2) {
-  return num1 / num2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-
-alert(result);
+// --- SIMPLE CALCULATOR --
+//
+// var add = function(num1, num2) {
+// 	return num1 + num2;
+// };
+//
+// var subtract = function(num1, num2) {
+//   return num1 - num2;
+// };
+//
+// var multiply = function(num1, num2) {
+//   return num1 * num2;
+// };
+//
+// var divide = function(num1, num2) {
+//   return num1 / num2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = divide(number1, number2);
+//
+// alert(result);
+//
+// var displayResult = function(info) {
+//   alert(info);
+// }
+//
+// -- CELSIUS TO FAHRENEHIT AND F TO C
+//
+// var fToC = function (num1) {
+//   return (num1 - 32) * 5 / 9;
+// }
+//
+// alert(fToC(100));
+//
+// -- PROMPT USER FOR FAVORITES AND RETURN STRING
+//
+// var yourAge = prompt("What is your age?");
+// var yourName = prompt("What is your name?");
+// var yourFood = prompt("What is your food?");
+//
+// var userInfo = function(age, name, food) {
+//   alert("Your silly name is " + name + " You are a geezer at " + age + " " + food + " is overrated...");
+// };
+//
+// userInfo(yourName, yourAge, yourFood);
+//
+// -- BMI CALCULATOR
+// var yourWeight = parseInt(prompt("What is your weight in KG?"));
+// var yourHeight = parseInt(prompt("What is your height in meters?"));
+//
+// var calcBMI = function(weight, height) {
+//   return weight / (height * height);
+// };
+//
+// alert(calcBMI(yourWeight, yourHeight));
+//
